459ed5dea6c716fca142831e016e19448bff61f6
Brad Thompson <bradleyglind@gmail.com>
2016-10-20 15:58:11 -0600

    Client - king and token class specified as kingtoken and citytoken

--- src/pages/game/Game.vue
+++ src/pages/game/Game.vue
@@ -21,10 +21,10 @@
           <div v-for="x in y" class="cell"
                v-on:mousedown="movestart(x.loc.x, x.loc.y)"
                v-on:touchstart="movestart(x.loc.x, x.loc.y)"
-               v-bind:class="{player: x.owner >= 0, solid: x.owner === -2, fog: x.owner === -3, empty: x.owner === -1 || x.owner === -4, me: x.owner === game.myid, highlight: x.highlight, moving: x.moving}"
+               v-bind:class="{player: x.owner >= 0, solid: x.owner === -2, fog: x.owner === -3, city: x.owner === -4, empty: x.owner === -1, me: x.owner === game.myid, highlight: x.highlight, moving: x.moving}"
                v-bind:style="{ backgroundColor: x.color }">
             <div class="tint"></div>
-            <div class="token" v-bind:class="{king: x.token === 1, city: x.token === 4}"></div>
+            <div class="token" v-bind:class="{kingtoken: x.token === 1, citytoken: x.token === 4}"></div>
             <div class="units" v-show="x.units>0">{{move.percent > 0 && x.move_help > 0 ? move.percent + '%':x.units}}</div>
           </div>
         </div>
@@ -305,6 +305,10 @@
           background-image: url('../../assets/map.jpg');
           background-size: cover;
         }
+        .city {
+          background-image: url('../../assets/cityback.jpg');
+          background-size: cover;
+        }
         .player {
           border: 2px solid grey;
         }
@@ -381,13 +385,13 @@
         pointer-events: none;
       }
 
-      .king {
+      .kingtoken {
         display: block;
         background-image: url('../../assets/crown.png');
         background-size: contain;
         background-position: center;
       }
-      .city {
+      .citytoken {
         display: block;
         background-image: url('../../assets/city.png');
         background-size: contain;
@@ -410,7 +414,7 @@
         animation-duration: 6s;
         animation-iteration-count: infinite;
 
-        .king {
+        .kingtoken {
           // my king
         }
       }
